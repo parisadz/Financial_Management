89a57416cbf6f8afe18ce4978c77c5463859b2a2
parisadz <khameneparisa@gmail.com>
2021-06-05 09:32:04 +0430

    sign up code done!

--- app/src/main/java/com/example/financialmanagement/LoginActivity.java
+++ app/src/main/java/com/example/financialmanagement/LoginActivity.java
@@ -1,14 +1,24 @@
 package com.example.financialmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 }
--- app/src/main/java/com/example/financialmanagement/SignupActivity.java
+++ app/src/main/java/com/example/financialmanagement/SignupActivity.java
@@ -1,14 +1,82 @@
 package com.example.financialmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.provider.FirebaseInitProvider;
 
 public class SignupActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    EditText namebox,mobilebox,emailbox,passwordbox;
+    Button loginbtn,singbtn;
+    FirebaseFirestore database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+
+        database = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        namebox = findViewById(R.id.namebox);
+        emailbox = findViewById(R.id.emailbox);
+        passwordbox = findViewById(R.id.passwordbox);
+
+        loginbtn = findViewById(R.id.loginbtn);
+        singbtn = findViewById(R.id.submitbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignupActivity.this,LoginActivity.class));
+            }
+        });
+
+        singbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email,pass,name,mobile;
+                email = emailbox.getText().toString();
+                pass = passwordbox.getText().toString();
+                name = namebox.getText().toString();
+                mobile = mobilebox.getText().toString();
+
+                final User user = new User();
+                user.setName(name);
+                user.setEmail(email);
+                user.setPassword(pass);
+
+                auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            database.collection("Users").document().set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    startActivity(new Intent(SignupActivity.this,LoginActivity.class));
+                                }
+                            });
+                            Toast.makeText(SignupActivity.this,"Your account is created!",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(SignupActivity.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
